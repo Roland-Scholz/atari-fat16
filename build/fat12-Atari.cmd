ddfc82b1b3b1fb79e0f4060bd0b93e263d746ed2
Roland Scholz <rolandscholz@gmx.de>
2024-12-22 21:10:19 +0100

    FAT-12 read

---
+++ build/fat12-Atari.cmd
@@ -0,0 +1,52 @@
+@echo off
+
+set PLATFORM=4
+call var-def.cmd
+
+
+call :compile_module fat16 0D00
+if NOT %result%==0 goto ende
+
+call :compile_module dup 1a40
+if NOT %result%==0 goto ende
+
+call :compile_module bootsec A800
+if NOT %result%==0 goto ende
+
+cd %REL%
+copy ..\%RES%\boot.atr .
+copy /Y /B fat16.com + dup.com dos.sys
+
+..\%TOOLS%\xfddos -i boot.atr dos.sys
+..\%TOOLS%\xfddos -i boot.atr bootsec.com
+
+echo on
+
+mkdir obj
+mkdir lst
+
+move *.lst lst > nul
+move *.o obj > nul
+move *.a obj > nul
+
+copy boot.atr e:
+
+
+:ende
+
+pause
+goto eof
+
+
+:compile_module
+%CC65%\ca65 -DPLATFORM=%PLATFORM% -l  %REL%\%1.lst %SRC%\%1.a65 -I %INC% -I %COMMON%\inc -o %REL%\%1.o
+set result=%ERRORLEVEL%
+
+if %result%==0 (
+
+	%CC65%\ld65 -t none %REL%\%1.o -o %REL%\%1.a
+	%CC65%\bin2hex %REL%\%1.a %REL%\%1.hex -o %2
+	java -jar %TOOLS%\Obj2Com\jar\ObjUtil.jar Obj2Com %REL%\%1.a %2
+)
+
+:eof
